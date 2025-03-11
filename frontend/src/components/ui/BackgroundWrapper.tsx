15c8bd2e7387361b684d5b60915d1508ace3aa28
Ali <aliadnani056@gmail.com>
2025-03-11 22:29:24 +0000

    relative path

--- frontend/src/components/ui/BackgroundWrapper.tsx
+++ frontend/src/components/ui/BackgroundWrapper.tsx
@@ -7,7 +7,7 @@ interface BackgroundWrapperProps {
 
 const BackgroundWrapper: React.FC<BackgroundWrapperProps> = ({ children }) => {
   return (
-    <div className="relative min-h-screen bg-[url('C:/Users/Lenovo/Desktop/PFS/SmartElectronicLibrary/frontend/src/assets/EXPORT-BG.png')] bg-no-repeat bg-cover">
+    <div className="relative min-h-screen bg-[url('assets/EXPORT-BG.png')] bg-no-repeat bg-cover">
       {children}
     </div>
   );
