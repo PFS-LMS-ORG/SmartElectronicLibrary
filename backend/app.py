073c691e31992756f5342a97336f4a7bc0b828e5
Adamo08 <omarelaaddam519@gmail.com>
2025-04-11 12:43:14 +0200

    Update app.py to specify host and port for Flask app execution

--- backend/app.py
+++ backend/app.py
@@ -4,4 +4,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Run the Flask app on port 5050
+    app.run(host='0.0.0.0', port=5050, debug=True)
