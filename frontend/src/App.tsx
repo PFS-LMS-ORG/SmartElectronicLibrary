3a61137bb4cbe864fcdd578eb81be0a0cd1eeed9
Adamo08 <omarelaaddam519@gmail.com>
2025-04-11 16:22:50 +0200

    Wrap application in AuthProvider and implement ProtectedRoutes for secure navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,68 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Search from "./pages/SearchPage";
-import Reservations from "./pages/Reservations";
-import Navbar from "./pages/Navbar";
-import BackgroundWrapper from "./components/ui/BackgroundWrapper";
-import LibraryLoginPage from "./pages/LibraryLoginPage"
-import LibraryRegistrationPage from "./pages/LibraryRegistrationPage"
-import BookDetails from "./pages/BookDetails";
-import Product from "./pages/Product";
+import { Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import Home from './pages/Home';
+import Search from './pages/SearchPage';
+import Reservations from './pages/Reservations';
+import Navbar from './pages/Navbar';
+import BackgroundWrapper from './components/ui/BackgroundWrapper';
+import LibraryLoginPage from './pages/LibraryLoginPage';
+import LibraryRegistrationPage from './pages/LibraryRegistrationPage';
+import BookDetails from './pages/BookDetails';
+import Product from './pages/Product';
 
 function App() {
   return (
-    <BackgroundWrapper>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/search" element={<Search />} />
-      <Route path="/login" element={<LibraryLoginPage />} />
-      <Route path="/register" element={<LibraryRegistrationPage />} />
-      <Route path="/reservations" element={<Reservations />} />
-      <Route path="/book/:id" element={<BookDetails />} />
-      <Route path="/product" element={<Product />} />
-      <Route path="*" element={<div>Page Not Found</div>} />
-    </Routes>
-    </BackgroundWrapper>
+    <AuthProvider>
+      <BackgroundWrapper>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<LibraryLoginPage />} />
+          <Route path="/register" element={<LibraryRegistrationPage />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/search"
+            element={
+              <ProtectedRoute>
+                <Search />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/reservations"
+            element={
+              <ProtectedRoute>
+                <Reservations />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/book/:id"
+            element={
+              <ProtectedRoute>
+                <BookDetails />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/product"
+            element={
+              <ProtectedRoute>
+                <Product />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<div className="container mx-auto px-8 py-8 text-white">Page Not Found</div>} />
+        </Routes>
+      </BackgroundWrapper>
+    </AuthProvider>
   );
 }
 
