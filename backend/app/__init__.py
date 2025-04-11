cb1cab2db4f0d014b649c6b66c4e2677699031b4
Adamo08 <omarelaaddam519@gmail.com>
2025-04-11 16:22:23 +0200

    Add authentication support by initializing Bcrypt and JWTManager, and register auth blueprint

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,11 @@
 # app/__init__.py
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 from app.db import db
 from flask_migrate import Migrate
 from app.controllers.book_controller import book_controller
+from app.controllers.auth_controller import auth_bp  # Import auth blueprint
 from dotenv import load_dotenv
 import os
 
@@ -12,10 +15,16 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')  # For JWT
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')  # Separate key for JWT
 
     db.init_app(app)
+    Bcrypt(app)  # Initialize Bcrypt
+    JWTManager(app)  # Initialize JWTManager
     Migrate(app, db)
 
+    # Register blueprints
     app.register_blueprint(book_controller)
+    app.register_blueprint(auth_bp, url_prefix='/auth')  # Register auth blueprint
 
     return app
