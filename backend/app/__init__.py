6e74e51986a3f6fbd0b775e748ec9c59a9abae04
Adamo08 <omarelaaddam519@gmail.com>
2025-04-11 14:06:06 +0200

    Refactor database URI configuration to use environment variable and clean up imports

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,18 +2,20 @@
 from flask import Flask
 from app.db import db
 from flask_migrate import Migrate
-from app.controllers.book_controller import book_controller  # Import your blueprint
-
+from app.controllers.book_controller import book_controller
+from dotenv import load_dotenv
+import os
 
 def create_app():
+    load_dotenv()  # Load environment variables from .env
+
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:4306/library_db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    db.init_app(app)  # Initialize the db
+    db.init_app(app)
     Migrate(app, db)
 
-    # Register the blueprint
     app.register_blueprint(book_controller)
 
     return app
